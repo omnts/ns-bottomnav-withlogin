d8ce464441bb5b217053f39c29fbb38db1b8a05a
Omar M <ommalago@Omars-MacBook-Air.local>
2020-01-31 16:18:18 +0100

    Lazy loading KO for search and browse page

--- src/app/modules/bottomnav/bottomnav-routing.module.ts
+++ src/app/modules/bottomnav/bottomnav-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
-import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { NativeScriptRouterModule, NSEmptyOutletComponent } from 'nativescript-angular/router';
 import { BottomnavComponent } from './pages/bottomnav.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "bottomnav", pathMatch: "full" },
+  
   { path: "bottomnav", redirectTo: "/(browse:browse//search:search)", pathMatch: "full" },
-  { path: "browse", outlet: "browse", loadChildren: () => import('../browse/browse.module').then(m => m.BrowseModule)},
-  { path: "search", outlet: "search" , loadChildren: () => import('../search/search.module').then(m => m.SearchModule)},
+  //{ path: "browse", outlet: "browse", loadChildren: () => import('../browse/browse.module').then(m => m.BrowseModule)},
+
+  { path: "search", component: NSEmptyOutletComponent, outlet: "search" , loadChildren: () => import('../search/search.module').then(m => m.SearchModule)},
+  //{ path: "search", outlet: "search" , loadChildren: () => import('../search/search.module').then(m => m.SearchModule)},
+
 ];
 
 @NgModule({
